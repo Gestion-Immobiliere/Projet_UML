c021db36e093ce0befad14211d72f0a021f9abf4
Djibril-Kane <djibrilkanediallo04@gmail.com>
2025-04-27 19:29:44 +0000

    Ajout des diverses fonctionnalités pour l'envoi des messages et test de ces fonctionnalités

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
+
+require __DIR__.'/auth.php';
+
+Route::get('chat', [ChatController::class, 'index']);
