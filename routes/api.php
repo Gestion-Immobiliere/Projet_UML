5a846e520d255d03504fd242c34b14ce7a0dbcd2
Djibril-Kane <djibrilkanediallo04@gmail.com>
2025-04-27 19:29:44 +0000

    Ajout des diverses fonctionnalités pour l'envoi des messages et test de ces fonctionnalités

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->post('message', [ChatController::class, 'store']);
+
+
+
+Route::post('login', [UserController::class, 'login']);
